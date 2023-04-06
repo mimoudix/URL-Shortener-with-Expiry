06df4cd4d447a669da6e02764404458126618366
Mohcine Mimoudi <mohcinemimoudi@gmail.com>
2023-04-06 09:36:33 +0000

    Update css path

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ require_once('config.php');
     <title>URL Shortener</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
-    <link rel="stylesheet" href="css/tailwind.css">
+    <link rel="stylesheet" href="output.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 
